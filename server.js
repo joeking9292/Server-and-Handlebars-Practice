6e805e3fb4053e3a287e144cbe7772e92b0b7fd1
Joseph Noonan <noonanj@flip2.engr.oregonstate.edu>
2018-11-28 17:46:32 -0800

    added express-handlebars and edited the file paths for the server

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
  * Write your routing code in this file.  Make sure to add your name and
  * @oregonstate.edu email address below.
  *
- * Name:
- * Email:
+ * Name: Joesph Noonan
+ * Email: noonanj@oregonstate.edu
  */
 
 var path = require('path');
@@ -19,12 +19,26 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
-app.get('/posts') //////////////////
+app.get('/posts', function (req, res, next) {
+    res.status(200).sendFile(__dirname + '/public/index.html');
+});
+
+app.get('posts/:post', function (req, res, next) {
+    var person = req.params.post;
+    if (postData[post]) {
+      res.render('postPage', peopleData[person]);
+    } else {
+      next();
+    }
+  });
 
 app.get('*', function(req, res) {
     res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 app.listen(port, function() {
+    if (err) {
+        throw err;
+    }
     console.log("== Server is listening on port", port);
 });
